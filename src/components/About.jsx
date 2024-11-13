864340cb220729bb0e22bad096ae31a3672a5f0e
Uday kumar M <136188719+JScoder4005@users.noreply.github.com>
2024-11-13 16:59:27 +0530

    UI fixing in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import Tilt from 'react-tilt'
+// import Tilt from "react-tilt";
 import {motion} from 'framer-motion'
 
+import { styles } from '../style'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+
+
+
+
+const serviceCard = ({icon,title, index}) => {
+  return (
+    <p>{title}</p>
+  )
+}
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.dev
+        variants={textVariant()}
+    >
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.dev>
+
+    <motion.p variants={fadeIn("", "", 0.1, 1)}
+    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+    >
+    I'm a skilled software developer with experience in Typescript and Javascript, and expertise
+        in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients 
+        to create efficient and scalable solutions that solve real-world problems. Let's work together to bring your ideas to life!
+    </motion.p>
+
+    <div className='mt-20 flex flex-wrap gap-10'>
+{services.map((service,index) => {
+          // eslint-disable-next-line react/no-unknown-property
+          <serviceCard key={service.title} index={index} {...service} />
+})}
+    </div>
+    </>
   )
 }
 

c02fe8c0ae6167e160197980691ac23c0e6e656d
JScoder4005 <muk4005@gmail.com>
2024-11-14 22:58:04 +0530

    Not getting too much time to work on this but will complete this. Added Tilt to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,45 +1,48 @@
-import React from 'react'
+import React from 'react';
 // import Tilt from "react-tilt";
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion';
 
-import { styles } from '../style'
-import { services } from '../constants'
-import { fadeIn, textVariant } from '../utils/motion'
+import { styles } from '../style';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { Tilt } from 'react-tilt';
 
-
-
-
-const serviceCard = ({icon,title, index}) => {
+const serviceCard = ({ icon, title, index }) => {
   return (
-    <p>{title}</p>
-  )
-}
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.dev
+        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      ></motion.dev>
+    </Tilt>
+  );
+};
 const About = () => {
   return (
     <>
-    <motion.dev
-        variants={textVariant()}
-    >
-      <p className={styles.sectionSubText}>Introduction</p>
-      <h2 className={styles.sectionHeadText}>Overview.</h2>
-    </motion.dev>
+      <motion.dev variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.dev>
 
-    <motion.p variants={fadeIn("", "", 0.1, 1)}
-    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-    >
-    I'm a skilled software developer with experience in Typescript and Javascript, and expertise
-        in frameworks like React, Node.js, and Three.js. I'm a quick learner and collaborate closely with clients 
-        to create efficient and scalable solutions that solve real-world problems. Let's work together to bring your ideas to life!
-    </motion.p>
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm a skilled software developer with experience in Typescript and
+        Javascript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient and scalable solutions that solve real-world problems.
+        Let's work together to bring your ideas to life!
+      </motion.p>
 
-    <div className='mt-20 flex flex-wrap gap-10'>
-{services.map((service,index) => {
-          // eslint-disable-next-line react/no-unknown-property
-          <serviceCard key={service.title} index={index} {...service} />
-})}
-    </div>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => {
+          return <serviceCard key={service.title} index={index} {...service} />;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;

1f4caf976d3ee301f61df7799808a832eab61c76
JScoder4005 <muk4005@gmail.com>
2024-11-18 22:54:04 +0530

    RE-working on About section due to some deps errors...

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { motion } from 'framer-motion';
-
-import { styles } from '../style';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
+// Correct import for `react-tilt`
 import { Tilt } from 'react-tilt';
-import { max } from 'three/examples/jsm/nodes/Nodes.js';
+import { styles } from '../../src/style';
+import { fadeIn, textVariant } from '../../src/utils/motion';
+import { services } from '../constants';
 
-const serviceCard = ({ icon, title, index }) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.dev
-        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-      ></motion.dev>
-    </Tilt>
-  );
-};
-const About = () => {
-  return (
-    <>
-      <motion.dev variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-
+      >
         <div
           options={{
             max: 45,
@@ -30,35 +21,33 @@ const About = () => {
             speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img
-            src={icon}
-            alt="web-development"
-            className="w-16 h-16 object-contain"
-          />
-
-          <h3 className="text-white text-[20px] font-bold text-center">
-            {title}
-          </h3>
-        </div>
+        ></div>
       </motion.dev>
-
+    </Tilt>
+  );
+};
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant}>
+        <p className={styles.sectionHeadText}>Introduction</p>
+        <h2>Overview.</h2>
+      </motion.div>
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in Typescript and
-        Javascript, and expertise in frameworks like React, Node.js, and
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient and scalable solutions that solve real-world problems.
-        Let's work together to bring your ideas to life!
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
-
-      {/* <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => {
-          return <serviceCard key={service.title} index={index} {...service} />;
+          <ServiceCard key={service.title} indwx={index} {...service} />;
         })}
-      </div> */}
+      </div>
     </>
   );
 };
